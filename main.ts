3cc08ee2b0af58a6ca44ab8d6a7f3df364079655
t3nth <93925698+t3nth@users.noreply.github.com>
2023-01-30 14:57:10 +0100

    program now works as expected

--- main.ts
+++ main.ts
@@ -12,29 +12,39 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     udn = 2
 })
+// sound if temperature not ideal
 loops.everyInterval(60000, function () {
     temperature = input.temperature()
     if (temperature < 17) {
         while (udn != 1) {
-            for (let index = 0; index < 4; index++) {
-                for (let index = 0; index < 3; index++) {
-                    music.playTone(262, music.beat(BeatFraction.Whole))
-                    music.playTone(262, music.beat(BeatFraction.Whole))
-                    music.playTone(262, music.beat(BeatFraction.Whole))
-                }
-                basic.pause(5000)
+            for (let index = 0; index < 3; index++) {
+                music.playTone(4000, music.beat(BeatFraction.Whole))
+                music.playTone(4200, music.beat(BeatFraction.Whole))
+                music.playTone(4400, music.beat(BeatFraction.Whole))
             }
+            basic.pause(5000)
+        }
+    } else {
+        while (udn != 2) {
+            for (let index = 0; index < 3; index++) {
+                music.playTone(4400, music.beat(BeatFraction.Whole))
+                music.playTone(4200, music.beat(BeatFraction.Whole))
+                music.playTone(4000, music.beat(BeatFraction.Whole))
+            }
+            basic.pause(5000)
+        }
+    }
+    if (temperature < 17) {
+        while (udn != 2) {
+            basic.showLeds(`
+                . # . . .
+                . # . # .
+                . # # # #
+                # # # # #
+                . # # # .
+                `)
         }
     } else if (temperature > 21) {
-        while (udn != 2) {
-            for (let index = 0; index < 4; index++) {
-                for (let index = 0; index < 3; index++) {
-                    music.playTone(262, music.beat(BeatFraction.Whole))
-                    music.playTone(262, music.beat(BeatFraction.Whole))
-                    music.playTone(262, music.beat(BeatFraction.Whole))
-                }
-                basic.pause(5000)
-            }
-        }
+    	
     }
 })
