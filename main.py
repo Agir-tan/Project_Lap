1964913a997ade5d66532b4de9eb2d2099f0660a
t3nth <93925698+t3nth@users.noreply.github.com>
2023-01-30 15:19:32 +0100

    added LEDs and animations

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ temperature = 0
 def on_button_pressed_a():
     global udn
     udn = 1
+    basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 # all
@@ -12,6 +13,7 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 def on_button_pressed_ab():
     global udn
     udn = 0
+    basic.clear_screen()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 # ignore high
@@ -19,25 +21,109 @@ input.on_button_pressed(Button.AB, on_button_pressed_ab)
 def on_button_pressed_b():
     global udn
     udn = 2
+    basic.clear_screen()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-#sound if temperature not ideal
+# sound if temperature not ideal
 
 def on_every_interval():
-    global temperature
+    global temperature, udn
     temperature = input.temperature()
     if temperature < 17:
         while udn != 1:
             for index in range(3):
-                music.play_tone(262, music.beat(BeatFraction.WHOLE))
-                music.play_tone(262, music.beat(BeatFraction.WHOLE))
-                music.play_tone(262, music.beat(BeatFraction.WHOLE))
+                music.play_tone(4000, music.beat(BeatFraction.WHOLE))
+                music.play_tone(4200, music.beat(BeatFraction.WHOLE))
+                music.play_tone(4400, music.beat(BeatFraction.WHOLE))
             basic.pause(5000)
     elif temperature > 21:
         while udn != 2:
             for index2 in range(3):
-                music.play_tone(262, music.beat(BeatFraction.WHOLE))
-                music.play_tone(262, music.beat(BeatFraction.WHOLE))
-                music.play_tone(262, music.beat(BeatFraction.WHOLE))
+                music.play_tone(4400, music.beat(BeatFraction.WHOLE))
+                music.play_tone(4200, music.beat(BeatFraction.WHOLE))
+                music.play_tone(4000, music.beat(BeatFraction.WHOLE))
             basic.pause(5000)
+    if temperature > 17 and temperature < 21:
+        udn = 0
 loops.every_interval(60000, on_every_interval)
+
+# leds if temperature not ideal
+
+def on_every_interval2():
+    global temperature
+    temperature = input.temperature()
+    if temperature < 17:
+        while udn != 1:
+            basic.show_leds("""
+                . . # . .
+                                . # # # .
+                                # . # . #
+                                . . # . .
+                                . . . . .
+            """)
+            basic.show_leds("""
+                . # # # .
+                                # . # . #
+                                . . # . .
+                                . . . . .
+                                . . # . .
+            """)
+            basic.show_leds("""
+                # . # . #
+                                . . # . .
+                                . . . . .
+                                . . # . .
+                                . # # # .
+            """)
+            basic.show_leds("""
+                . . # . .
+                                . . . . .
+                                . . # . .
+                                . # # # .
+                                # . # . #
+            """)
+            basic.show_leds("""
+                . . . . .
+                                . . # . .
+                                . # # # .
+                                # . # . #
+                                . . # . .
+            """)
+    elif temperature > 21:
+        while udn != 2:
+            basic.show_leds("""
+                . . . . .
+                                . . # . .
+                                # . # . #
+                                . # # # .
+                                . . # . .
+            """)
+            basic.show_leds("""
+                . . # . .
+                                . . . . .
+                                . . # . .
+                                # . # . #
+                                . # # # .
+            """)
+            basic.show_leds("""
+                . # # # .
+                                . . # . .
+                                . . . . .
+                                . . # . .
+                                # . # . #
+            """)
+            basic.show_leds("""
+                # . # . #
+                                . # # # .
+                                . . # . .
+                                . . . . .
+                                . . # . .
+            """)
+            basic.show_leds("""
+                . . # . .
+                                # . # . #
+                                . # # # .
+                                . . # . .
+                                . . . . .
+            """)
+loops.every_interval(60000, on_every_interval2)
--- main.ts
+++ main.ts
@@ -3,14 +3,98 @@ let temperature = 0
 // ignore low
 input.onButtonPressed(Button.A, function () {
     udn = 1
+    basic.clearScreen()
 })
 // all
 input.onButtonPressed(Button.AB, function () {
     udn = 0
+    basic.clearScreen()
 })
 // ignore high
 input.onButtonPressed(Button.B, function () {
     udn = 2
+    basic.clearScreen()
+})
+// leds if temperature not ideal
+loops.everyInterval(60000, function () {
+    temperature = input.temperature()
+    if (temperature < 17) {
+        while (udn != 1) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                . . . . .
+                `)
+            basic.showLeds(`
+                . # # # .
+                # . # . #
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
+            basic.showLeds(`
+                # . # . #
+                . . # . .
+                . . . . .
+                . . # . .
+                . # # # .
+                `)
+            basic.showLeds(`
+                . . # . .
+                . . . . .
+                . . # . .
+                . # # # .
+                # . # . #
+                `)
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                . # # # .
+                # . # . #
+                . . # . .
+                `)
+        }
+    } else if (temperature > 21) {
+        while (udn != 2) {
+            basic.showLeds(`
+                . . . . .
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . # . .
+                . . . . .
+                . . # . .
+                # . # . #
+                . # # # .
+                `)
+            basic.showLeds(`
+                . # # # .
+                . . # . .
+                . . . . .
+                . . # . .
+                # . # . #
+                `)
+            basic.showLeds(`
+                # . # . #
+                . # # # .
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
+            basic.showLeds(`
+                . . # . .
+                # . # . #
+                . # # # .
+                . . # . .
+                . . . . .
+                `)
+        }
+    }
 })
 // sound if temperature not ideal
 loops.everyInterval(60000, function () {
@@ -18,33 +102,23 @@ loops.everyInterval(60000, function () {
     if (temperature < 17) {
         while (udn != 1) {
             for (let index = 0; index < 3; index++) {
-                music.playTone(4000, music.beat(BeatFraction.Whole))
-                music.playTone(4200, music.beat(BeatFraction.Whole))
-                music.playTone(4400, music.beat(BeatFraction.Whole))
+                music.playTone(262, music.beat(BeatFraction.Whole))
+                music.playTone(262, music.beat(BeatFraction.Whole))
+                music.playTone(262, music.beat(BeatFraction.Whole))
             }
             basic.pause(5000)
         }
-    } else {
+    } else if (temperature > 21) {
         while (udn != 2) {
             for (let index = 0; index < 3; index++) {
-                music.playTone(4400, music.beat(BeatFraction.Whole))
-                music.playTone(4200, music.beat(BeatFraction.Whole))
-                music.playTone(4000, music.beat(BeatFraction.Whole))
+                music.playTone(262, music.beat(BeatFraction.Whole))
+                music.playTone(262, music.beat(BeatFraction.Whole))
+                music.playTone(262, music.beat(BeatFraction.Whole))
             }
             basic.pause(5000)
         }
     }
-    if (temperature < 17) {
-        while (udn != 2) {
-            basic.showLeds(`
-                . # . . .
-                . # . # .
-                . # # # #
-                # # # # #
-                . # # # .
-                `)
-        }
-    } else if (temperature > 21) {
-    	
+    if (temperature > 17 && temperature < 21) {
+        udn = 0
     }
 })
